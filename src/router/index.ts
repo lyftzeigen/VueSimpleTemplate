78802e8b8888ba5b7e2f19afeaee01811a4546e7
Pavel Vasiliev <lyftzeigen@mail.ru>
2022-11-01 15:22:04 +0300

    Fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,14 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'main',
-      component: () => import('../pages/Main.vue')
+      path: "/",
+      name: "main",
+      component: () => import("../pages/MainView.vue"),
     },
-  ]
+  ],
 })
 
 export default router
